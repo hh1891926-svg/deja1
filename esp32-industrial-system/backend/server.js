f178c5e37c588bd80602d0680ddb169786f63759
sakr374 <eiadahmed202205999@gmail.com>
2025-12-18 03:10:00 +0200

    edit the middleware in the server

--- esp32-industrial-system/backend/server.js
+++ esp32-industrial-system/backend/server.js
@@ -9,7 +9,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "https://app.dejaa.site" 
+}));
 app.use(bodyParser.json());
 
 // MongoDB Connection
